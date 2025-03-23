8fb30a967c3a1b9b91cb3c65c5afe64e5d4fa0eb
MeSonali <bhumikag437@gmail.com>
2025-03-23 16:07:11 +0530

    added database

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,14 @@ import { Product } from "./entities/Product"; // ✅ Ensure the correct path
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "1234",
+  host: "http://gateway01.ap-southeast-1.prod.aws.tidbcloud.com/",
+  port: 4000,
+  username: "vonnB2CeZAEgprf.root",
+  password: "FjMpnW4Btv1w6DkR",
   database: "Ecommerce", // ✅ Change this
   synchronize: true,
   logging: true,
   entities: [Product],
 });
+
+
